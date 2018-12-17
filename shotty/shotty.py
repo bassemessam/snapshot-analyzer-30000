eac8031bd4ac89f6da4d646adf4f2e31708eca4a
Essam <bassem.essam@nokia.com>
2018-12-17 03:40:37 +0200

    adding some features to the script to collect more data from EC2

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,22 @@
 import boto3
+import click
 
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
+session = boto3.Session(profile_name='s3-session')
+ec2 = session.resource('ec2')
 
+@click.command()
+
+def ec2_instances_list():
+    "List the EC2 instances"
     for i in ec2.instances.all():
-        print(i)
+        #print (ec2.instances.all())
+        print(','.join((i.id,
+        i.instance_type,
+        i.placement['AvailabilityZone'],
+        i.state['Name'],
+        i.public_dns_name)))
+
+    return
+
+if __name__ == '__main__' :
+    ec2_instances_list()
